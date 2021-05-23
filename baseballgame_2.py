ae78298bae4edcab3deff9ed93763bb232a3e379
dlfma1223 <sunsss123@naver.com>
2021-05-23 18:11:48 +0900

    [Text]  - player1, 2 나눠서 게임 할 수 있도록 지정.  - 볼넷 시, 스크라이크로 구현.  - 스트라이크 시, 베이스 진루하도록 구현. 홈 도착 시, 점수 증가.  - 두 플레이어 중 먼저 3점을 얻는 사람이 이기도록 구현.

--- baseballgame_2.py
+++ baseballgame_2.py
@@ -23,63 +23,87 @@ cntPlayer1 = 0
 cntPlayer2 = 0
 
 # 게임진행
-while player1Base == 3 or player2Base == 3:
+while cntPlayer1 < 3 and cntPlayer2 < 3:
     strikecnt = 0
     ballcnt = 0
     guess = []
 
-    for i in range(3):
-        num = int(input("{}, 1~9까지 숫자를 입력하세요:".format(i)))
-        guess.append(num)
+    answer = random.sample(range(1, 10), 4)
+    print("정답은=", answer)
 
-        print(guess)
+    for i in range(4):
+        input_num = int(input("{}, 1~9까지 숫자를 입력하세요:".format(i)))
+        guess.append(input_num)
 
-    for i in range(3):
+    for i in range(4):
         if guess[i] == answer[i]:
             strikecnt = strikecnt + 1
         elif guess[i] in answer:
             ballcnt += 1
+
     print("strike = {}, ball = {}".format(strikecnt, ballcnt))
 
-    cnt += 1
+    if isPlayer1 == True:
+        if tmpPlayer1Ball + ballcnt > 0 or strikecnt > 0:
+            if (tmpPlayer1Ball + ballcnt) / 4 > 0:
+                player1Base += math.trunc((tmpPlayer1Ball + ballcnt) / 4)
+                # print("베이스 : {}".format(player1Base))
+                tmpPlayer1Ball = (tmpPlayer1Ball + ballcnt) % 4
+            if strikecnt > 0:
+                player1Base = + strikecnt
+            if player1Base >= 4:
+                cntPlayer1 += math.trunc(player1Base / 4)
+                player1Base = player1Base % 4
 
-    if player1Base < 4:
-        if ballcnt > 0:
-            player1Base += (ballcnt + tmpPlayer1Ball) / 4
-            tmpPlayer1Ball += (ballcnt + tempPlayer1Ball) % 4
-        elif strikecnt > 0:
-            player1Base += strikecnt
-    elif player1Base >= 4:
-        cntPlayer1 += player1Base / 4
-        player1Base -= 4 * (player1Base / 4)
-    elif isPlayer2 == True and isPlayer1 == False:
-        if player2Base < 4:
-            if ballcnt > 0:
-                player2Base += (ballcnt + tmpPlayer2Ball) / 4
-                tmpPlayer2Ball += (ballcnt + tempPlayer2Ball) % 4
-            elif strikecnt > 0:
-                player2Base += strikecnt
-        elif player2Base >= 4:
-            cntPlayer2 += player2Base / 4
-            player2Base -= 4 * (player2Base / 4)
+        print("Player1 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(
+            player1Base, strikecnt, ballcnt, tmpPlayer1Ball, cntPlayer1))
+        isPlayer1 = False
+        isPlayer2 = True
+    elif isPlayer2 == True:
+        if tmpPlayer2Ball + ballcnt > 0 or strikecnt > 0:
+            if (tmpPlayer2Ball + ballcnt) / 4 > 0:
+                player2Base += math.trunc((tmpPlayer2Ball + ballcnt) / 4)
+                # print("베이스 : {}".format(player1Base))
+                tmpPlayer2Ball = (tmpPlayer2Ball + ballcnt) % 4
+            if strikecnt > 0:
+                player2Base = + strikecnt
+            if player2Base >= 4:
+                cntPlayer2 += math.trunc(player2Base / 4)
+                player2Base = player2Base % 4
+
+        print("Player2 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(
+            player1Base, strikecnt, ballcnt, tmpPlayer1Ball, cntPlayer1))
+        isPlayer1 = True
+        isPlayer2 = False
+
+    if cntPlayer1 == 3:
+        print("player1이 승리하였습니다.")
+    elif cntPlayer2 == 3:
+        print("player2가 승리하였습니다.")
+
+#     if player1Base < 4:
+#         if ballcnt > 0:
+#             player1Base += (ballcnt + tmpPlayer1Ball)/4
+#             tmpPlayer1Ball += (ballcnt + tmpPlayer1Ball)%4
+#         elif strikecnt > 0:
+#             player1Base += strikecnt
 
 
-def checkValue():
-    # 초기화
+#     elif player1Base >= 4 and :
+#         cntPlayer1 = cntPlayer1 + player1Base/4
+#         player1Base -= 4*(player1Base/4)
 
-    # for i in range(4):
-    for i in range(4):
-        guess.append(int(qle.text()[i]))
-    # num = int(input("{}, 1~9까지 숫자를 입력하세요:".format(i)))
-    # self.guess.append(int(self.qle.text()))
-    # self.guess = list()
-    # print(guess)
 
-    for i in range(4):
-        if guess[i] == value[i]:
-            strikecnt = strikecnt + 1
-        elif guess[i] in value:
-            ballcnt += 1
+#     elif isPlayer2 == True and isPlayer1 == False:
+#         if player2Base < 4:
+#             if ballcnt > 0:
+#                 player2Base += (ballcnt + tmpPlayer2Ball) / 4
+#                 tmpPlayer2Ball += (ballcnt + tmpPlayer2Ball) % 4
+#             elif strikecnt > 0:
+#                 player2Base += strikecnt
+#         elif player2Base >= 4:
+#             cntPlayer2 += player2Base / 4
+#             player2Base -= 4 * (player2Base / 4)
 
-    # self.lbl2.setText(str(self.guess))
+
 # print("{}번 만에 정답".format(cnt))
